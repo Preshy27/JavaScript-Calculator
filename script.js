6aa41e2bcd986eb46ece6170419f9526a1da5147
Precious Aienobe <105337990+Preshy27@users.noreply.github.com>
2022-06-21 09:19:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let num1 = prompt("Enter first number");
 let num2 = prompt("Enter second number");
-let operator = prompt("Choose an operator: +, -, *, /")
+let operator = prompt("Choose an operator: +, -, *, /");
 
 num1 = parseFloat(num1);
 num2 = parseFloat(num2);
